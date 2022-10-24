05540a46aa0d608e045e1dd8c11ae153b2c71e56
santiago <santiagovillahermosa@gmail.com>
2022-10-24 17:30:12 -0400

    routes changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import Home from './pages/home/Home'
 import Register from './pages/register/Register'
 import Login from './pages/login/Login'
 import Profile from './pages/profile/Profile'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter,HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext.js";
 
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={user ? <Home /> : <Login />}/>
         <Route path="/login" element={user ? <Navigate to='/home'/> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/login" /> : <Register />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
